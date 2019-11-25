3a115eeef136dcdc8b930f5a00c4beafde247f81
bakenray <lpqstrong@163.com>
2019-11-25 17:18:33 +0800

    vuex 分离配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import moduleA from './modules/moduleA'
+import moduleB from './modules/moduleB'
+import mutations from './mutations'
+import actions from './actions'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    groups:[1]
   },
-  mutations: {
-  },
-  actions: {
-  },
+  mutations,
+  actions,
   modules: {
+    moduleA,
+    moduleB
+  },
+  getters:{
+    getGroups(state){
+      return state.groups
+    }
   }
 })
