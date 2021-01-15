9c2a856c6649c0906dcccbd47411e3af8834fe29
bakenray <lpqstrong@163.com>
2021-01-15 15:29:03 +0800

    组件 props

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>Components Demo</div>
+  <div>
+    <h3>Components Text</h3>
+    <p href="#" v-show="disabled"> let me see see</p>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props:{
+    size:{
+      type:String,
+      default:'default'
+    },
+    disabled:{
+      type:Boolean,
+      default:false,
+      require:true   //必传属性
+    },
+    obj:{
+      type:Object,
+      default:function(){  //对象或数组必须从一个工厂函数获取
+        return {}  
+      }
+    }
+  }
 }
 
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,19 @@
   <div class="home">
       <img alt="Vue logo" src="../assets/logo.png">
       <h1>vue项目最小配置</h1>
+
+      <Coms :disabled="true"/>
+
   </div>
 </template>
 
 <script>
+
+import Coms from '@com/Demo.vue'
+
 export default {
   name: 'home',
+  components:{Coms}
 }
 </script>
 <style lang="scss" >
